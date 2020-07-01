a1033fb67c11f13b5064fea62cb590965a6243d5
Jose Donaldo LG <patto19123@gmail.com>
2020-07-01 18:13:08 -0500

    Uso y ejemplos de los pipes en angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre:string = 'capitan America';
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  PI : number = Math.PI;
+  Porcentaje: number = 0.234;
+  salario: number = 1234.5;
+
+  valorpromesa = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve('llego la data');
+    }, 2000);
+  })
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'primera',
+      casa: 20
+    }
+  }
 }
